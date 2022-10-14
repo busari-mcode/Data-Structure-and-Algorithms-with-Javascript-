76edcf10b85518c20337eb9b257db238a84e8ad4
BMdirac <busarimoruf20@gmail.com>
2022-10-14 18:05:45 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+//********* Creating Arrays *****/
+// var numbers = []; // declaring empty array
+// var numbers = [1,2,3,4,5]; // declaring an array variable with a set of elements
+
+
+// You can also create an array by calling the Array constructor:
+// var numbers = new Array();
+// var numbers = new Array(1,2,3,4,5); // set of elements as arguments to the constructor
+
+// var numbers = new Array(1,2,3,4,5);
+// console.log(numbers.length); // displays 5
+
+// Specifying one element as arguments serves as its length
+var numbers = new Array(10);
+console.log(numbers.length); // displays 10
+
+//************ Revision 2 ******/
+// let number = [1, 2, 3, 4, 5];
+
+// for (i = 0; i < number.length; i++) {
+//     console.log(number[i])
+// }
+
+// for (value of number) {
+//     console.log(value)
+// }
+
+//************ Revision 1 ******/
+// let myArray = [];
+
+// myArray.push(3);
+
+// console.log(myArray);
+
