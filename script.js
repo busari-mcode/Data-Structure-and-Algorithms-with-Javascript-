dd233cbe349a5b2ce4d9c5c4e81459e6bfd20608
BMdirac <busarimoruf20@gmail.com>
2023-01-11 14:39:13 +0100

    new changes

--- script.js
+++ script.js
@@ -1,3 +1,92 @@
+// *************** LEARNING PHASE 8 ****************************************
+// *************** Iterator Functions That Return a New Array *******************
+/*
+There are two iterator functions that return new arrays: map() and filter(). The map()
+function works like the forEach() function, applying a function to each element of an
+array. The difference between the two functions is that map() returns a new array with
+the results of the function application. Here is an example:
+*/
+// function curve(grade) {
+//    return grade += 5;
+//    }
+//    var grades = [77, 65, 81, 92, 83];
+//    var newgrades = grades.map(curve);
+//    console.log(newgrades); // [ 82, 70, 86, 97, 88 ]
+
+// Here is an example using strings:
+// function first(word) {
+//    return word[0];
+//    }
+//    var words = ["for","your","information"];
+//    var acronym = words.map(first);
+//    console.log(acronym.join("")); // displays: fyi
+
+/*
+The filter() function works similarly to every(), but instead of returning true if all
+the elements of an array satisfy a Boolean function, the function returns a new array
+consisting of those elements that satisfy the Boolean function. Here is an example:
+*/
+// function isEven(num) {
+//    return num % 2 == 0;
+//    }
+//    function isOdd(num) {
+//    return num % 2 != 0;
+//    }
+//    var nums = [];
+//    for (var i = 0; i < 20; ++i) {
+//    nums[i] = i+1;
+//    }
+//    var evens = nums.filter(isEven);
+//    console.log("Even numbers: ");
+//    console.log(evens); // [ 2,  4,  6,  8, 10, 12, 14, 16, 18, 20 ]
+//    var odds = nums.filter(isOdd);
+//    console.log("Odd numbers: ");
+//    console.log(odds); // [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19 ]
+
+// Here is another interesting use of filter():
+// function passing(num) {
+//    return num >= 60;
+// }
+//    var grades = [];
+//    for (var i = 0; i < 20; ++i) {
+//    grades[i] = Math.floor(Math.random() * 101);
+// }
+// var passGrades = grades.filter(passing);
+// console.log("All grades: ");
+// console.log(grades); 
+// console.log("Passing grades: ");
+// console.log(passGrades);
+
+/*
+Of course, we can also use filter() with strings. Here is an example that applies the
+spelling rule “i before e except after c”:
+*/
+// function afterc(str) {
+//    if (str.indexOf("cie") > -1) {
+//    return true;
+//    }
+//    return false;
+//    }
+//    var words = ["recieve","deceive","percieve","deceit","concieve"];
+//    var misspelled = words.filter(afterc);
+//    console.log(misspelled); // displays: [ 'recieve', 'percieve', 'concieve' ]
+
+
+// *************** Revision ****************************************
+// ***** string to array (using split() function)******
+// var sentence = "the quick brown Fox";
+// var myArray = sentence.split(" ");
+// console.log(myArray)
+
+// ***** array to string (using reduce() function)******
+// function add(currentConcat, nextWord) {
+//    return currentConcat + nextWord;
+// }
+
+// var myArray = ["the ", "quick ", "brown ", "fox "];
+// var myString = myArray.reduce(add);
+// console.log(myString)
+
 // *************** LEARNING PHASE 7 ****************************************
 // *************** Iterator Functions **************************************
 // *************** Non–Array-Generating Iterator Functions *******************
